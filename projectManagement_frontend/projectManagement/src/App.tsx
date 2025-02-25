5daf42a8513de20c2934560480d9d6b033a06d9b
PetraElgemyr <petra.elgemyr@gmail.com>
2025-02-25 14:35:14 +0100

    create appcontext hook and implement basic appcontext frontend

--- projectManagement_frontend/projectManagement/src/App.tsx
+++ projectManagement_frontend/projectManagement/src/App.tsx
@@ -1,11 +1,32 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import { AppContext } from "./components/contexts/AppContext";
 import { Routing } from "./Routing";
+import { getAllProjects } from "./services/projectServices";
+import { IProject } from "./interfaces/IProject";
 
 function App() {
+  const [projects, setProjects] = useState<IProject[]>([]);
+
+  const loadProjects = async () => {
+    const response = await getAllProjects();
+    setProjects(response);
+  };
+
+  useEffect(() => {
+    loadProjects();
+  }, []);
+
+  const contextValue = {
+    projects,
+    setProjects,
+  };
+
   return (
     <>
-      <div>Alla projekt</div>
-      <Routing></Routing>
+      <AppContext.Provider value={contextValue}>
+        <Routing />
+      </AppContext.Provider>
     </>
   );
 }
