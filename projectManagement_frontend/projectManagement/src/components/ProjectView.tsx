acff3fdf5db174ad71f2eddbac6d2037c322eeb6
PetraElgemyr <petra.elgemyr@gmail.com>
2025-02-27 14:25:05 +0100

    reload when going back to projectview from projectservicesform

--- projectManagement_frontend/projectManagement/src/components/ProjectView.tsx
+++ projectManagement_frontend/projectManagement/src/components/ProjectView.tsx
@@ -159,6 +159,7 @@ export const ProjectView = ({
               <Button
                 onClick={() => {
                   setIsEditProjectServicesMode(false);
+                  fetchServicesForProject();
                 }}
               >
                 Avbryt
