cf58f3d2e03ebdc9e57a606bcc4c7e6be17e608e
PetraElgemyr <petra.elgemyr@gmail.com>
2025-02-27 13:01:52 +0100

    add servicename to front interface

--- projectManagement_frontend/projectManagement/src/interfaces/IService.ts
+++ projectManagement_frontend/projectManagement/src/interfaces/IService.ts
@@ -1,11 +1,13 @@
 export interface IService {
   id: number;
   serviceType: string;
+  serviceName: string;
   hourlyCost: number;
 }
 
 export const emptyService = {
   id: 0,
   serviceType: "",
+  serviceName: "",
   hourlyCost: 0,
 };
