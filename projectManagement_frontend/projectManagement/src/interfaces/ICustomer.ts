758fd5a64df6e50b7daada832d45b600a4151e13
PetraElgemyr <petra.elgemyr@gmail.com>
2025-02-27 13:18:47 +0100

    create default empty interfaces objects

--- projectManagement_frontend/projectManagement/src/interfaces/ICustomer.ts
+++ projectManagement_frontend/projectManagement/src/interfaces/ICustomer.ts
@@ -1,7 +1,13 @@
-import { ICustomerType } from "./ICustomerType";
+import { defaultCustomerType, ICustomerType } from "./ICustomerType";
 
 export interface ICustomer {
   id: number;
   customerName: string;
   customerType: ICustomerType;
 }
+
+export const defaultCustomer: ICustomer = {
+  id: 0,
+  customerName: "",
+  customerType: defaultCustomerType,
+};
--- projectManagement_frontend/projectManagement/src/interfaces/ICustomerType.ts
+++ projectManagement_frontend/projectManagement/src/interfaces/ICustomerType.ts
@@ -2,3 +2,8 @@ export interface ICustomerType {
   id: number;
   customerTypeName: string;
 }
+
+export const defaultCustomerType: ICustomerType = {
+  id: 0,
+  customerTypeName: "",
+};
