9e6e6b7ea49568b24a70b42c1702122d8c124279
PetraElgemyr <petra.elgemyr@gmail.com>
2025-02-27 14:01:37 +0100

    remove comment, create default projectmanager

--- projectManagement_frontend/projectManagement/src/interfaces/IProjectManager.ts
+++ projectManagement_frontend/projectManagement/src/interfaces/IProjectManager.ts
@@ -2,3 +2,8 @@ export interface IProjectManager {
   id: number;
   name: string;
 }
+
+export const defaultProjectManager: IProjectManager = {
+  id: 0,
+  name: "",
+};
--- projectManagement_frontend/projectManagement/src/services/userServices.ts
+++ projectManagement_frontend/projectManagement/src/services/userServices.ts
@@ -5,7 +5,6 @@ export const getAllProjectManagers = async (): Promise<IProjectManager[]> => {
   const response = await axios.get(
     "https://localhost:7043/api/users/projectmanagers"
   );
-  console.log(response.data);
 
   return response.data;
 };
